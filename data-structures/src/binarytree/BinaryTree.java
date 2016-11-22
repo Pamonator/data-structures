b44f7465a428c90e51d970aa61b547263d5f6705
a1402072 <a1402072@LAB111M20.lab111.campus.ifsp>
2016-11-22 20:39:06 -0200

    metodos showLeaves() leafCount() nodeSum() deepness()

--- data-structures/src/binarytree/BinaryTree.java
+++ data-structures/src/binarytree/BinaryTree.java
@@ -86,8 +86,67 @@ public class BinaryTree {
     }
 
     public void showLeaves(TreeNode temp) {
-        
-        
-        
+
+        if (temp.getLeft() != null && temp.getRight() != null) {
+
+            showLeaves(temp.getLeft());
+            showLeaves(temp.getRight());
+
+        } else {
+
+            System.out.println(temp.getValue());
+
+        }
     }
+
+    public int leafCount(TreeNode temp) {
+
+        if (temp.getLeft() == null && temp.getRight() == null) {
+
+            return 1;
+
+        } else {
+
+            return leafCount(temp.getLeft()) + leafCount(temp.getRight());
+
+        }
+
+    }
+
+    public int nodeCount(TreeNode temp) {
+
+        if (temp.getLeft() == null && temp.getRight() == null) {
+
+            return 0;
+
+        }
+
+        return 1 + nodeCount(temp.getLeft()) + nodeCount(temp.getRight());
+
+    }
+
+    public int sumValues(TreeNode temp) {
+
+        if (temp.getLeft() == null && temp.getRight() == null) {
+
+            return 0;
+
+        }
+
+        return temp.getValue() + sumValues(temp.getLeft()) + sumValues(temp.getRight());
+
+    }
+
+    public int deepness(TreeNode temp) {
+
+        if (temp.getLeft() == null && temp.getRight() == null) {
+
+            return 0;
+
+        }
+
+        return 1 + deepness(temp.getLeft()) + deepness(temp.getRight());
+
+    }
+
 }
--- data-structures/src/main/BinaryTreeMain.java
+++ data-structures/src/main/BinaryTreeMain.java
@@ -18,33 +18,18 @@ public class BinaryTreeMain {
 
         BinaryTree binaryTree = new BinaryTree();
         Scanner scanner = new Scanner(System.in);
-        int flag;
+        int values[] = {28, 6, 12, 19, 36, 5, 13, 30, 57};
         int value;
 
-        System.out.println("### Welcome to the Binary Tree Tabajara Tester ###");
-
-        do {
-
-            System.out.println("\n# Escolha sua opçao:"
-                    + "\n#1  - Adicionar um valor inteiro."
-                    + "\n#2  - Exibir os valores da lista."
-                    + "\n#0  - Sair.");
-            flag = scanner.nextInt();
-
-            switch (flag) {
-                case 1:
-                    System.out.println("Digite o valor de deseja adicionar na árvore: ");
-                    value = scanner.nextInt();
-                    binaryTree.insert(value);
-                    break;
-                case 2:
-                    binaryTree.showTree(binaryTree.getRoot());
-                    break;
-
-            }
-
-        } while (flag != 0);
-
+        for (int i = 0; i < values.length; i++) {
+            
+            binaryTree.insert(values[i]);
+            
+        }
+        
+        binaryTree.showLeaves(binaryTree.getRoot());
+        
+        
+        System.out.println("numero de folhas: " + binaryTree.leafCount(binaryTree.getRoot()));
     }
-
 }
